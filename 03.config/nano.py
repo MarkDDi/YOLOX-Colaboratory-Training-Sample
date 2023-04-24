7a941bac3581cb9e560d8310b4909383f4f11ead
KangHui <KangHui@szmtc.com.cn>
2023-04-24 09:51:46 +0800

    增加AP_IPGMundoGO

--- 03.config/nano.py
+++ 03.config/nano.py
@@ -28,7 +28,7 @@ class Exp(MyExp):
         self.val_ann = "/content/dataset/annotations/validation_annotations.json"
 
         #要根据02.annotation_data\pascal_label_map.pbtxt中模型的数量来修改，如id: 5，则此处也要改成5
-        self.num_classes = 16
+        self.num_classes = 17
 
         self.max_epoch = 500
         self.data_num_workers = 4
